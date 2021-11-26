6410e2b055c0fcd51d7a45cf99130cd9d7d4e914
Vanon Borget <vanon.borget@mmibordeaux.com>
2021-11-26 16:06:25 +0100

    input latitude & longitude

--- src/routes/new-spot.js
+++ src/routes/new-spot.js
@@ -12,17 +12,26 @@ function NewSpot() {
 
         // Initialisation de la variable qui va contenir les valeurs du form
         let formResult = {}
-        
+
         // Parcours le tableau des éléments HTML du form
         for (let i = 0; i < event.target.length; i++) {
             const element = event.target[i];
             console.log(element)
             if (element.nodeName == 'INPUT') {
-                //Ajoute à formResult les valeurs trouvés dans les inputs du form
-                formResult[element.name] = element.value
+                
+                    //Transformer Longitude et latitude en number pour + lisible pour mapbox
+                if (element.name == 'longitude' || element.name == "latitude") {
+                    formResult[element.name] = Number(element.value)
+                } else {
+                    //Ajoute à formResult les valeurs trouvés dans les inputs du form
+                    formResult[element.name] = element.value
+                }
+
             }
         }
-        // Ajoute à la const setSpots 
+
+
+        // Ajoute à la const setSpots les données de formResult
         setSpots([...spots, formResult])
         // console.log(formResult)
     }
@@ -46,10 +55,16 @@ function NewSpot() {
                     <input type="text" name="city" id="city" />
                 </div>
                 <div>
-                    <label htmlFor="city">
+                    <label htmlFor="longitude">
                         Longitude
                     </label>
                     <input type="text" name="longitude" id="longitude" />
+                </div>
+                <div>
+                    <label htmlFor="latitude">
+                        Latitude
+                    </label>
+                    <input type="text" name="latitude" id="latitude" />
                 </div>
                 <button type="submit">Créer mon spot</button>
             </form>
