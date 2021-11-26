9bbcd7a83d0fff38d160ee359ab393bdeb2ffc2c
Vanon Borget <vanon.borget@mmibordeaux.com>
2021-11-26 09:30:08 +0100

    linked pin with cards

--- src/App/index.js
+++ src/App/index.js
@@ -38,13 +38,18 @@ const App = () => {
       <Header />
       <Grid container spacing={3} style={{ width:'100%' }}>
         <Grid item xs={12} md={4}>
-          <List places={places} />
+          <List 
+            places={places}
+
+
+          />
         </Grid>
         <Grid item xs={12} md={8}>
           <HomeMap 
             setCoordinates={setCoordinates}
             setBounds={setBounds}
             coordinates={coordinates}
+            places={places}
           />
         </Grid>
       </Grid>
--- src/components/HomeMap/index.js
+++ src/components/HomeMap/index.js
@@ -4,12 +4,12 @@ import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
 import React, { useState, useEffect, useCallback, useRef } from "react";
 import ReactMapGL, { Marker, Popup, FullscreenControl, GeolocateControl, NavigationControl } from "react-map-gl";
 import { Paper, Typography, useMediaQuery } from "@material-ui/core";
-import LocationOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
 import Rating from "@material-ui/lab";
 
 import useStyles from './styles';
 
-export default function HomeMap({ setCoordinates, setBounds, coordinates }) {
+export default function HomeMap({ setCoordinates, setBounds, coordinates, places }) {
   const classes = useStyles();
   const isMobile = useMediaQuery('(min-width:600px)');
   const [viewport, setViewport] = useState({
@@ -80,8 +80,33 @@ export default function HomeMap({ setCoordinates, setBounds, coordinates }) {
         }}
         ref={mapRef}
         onViewportChange={handleViewportChange}
-      >
+      > 
         <FullscreenControl style={fullscreenControlStyle} />
+         {places?.map((place, i) =>(
+            <div
+                // className={classes.markerContainer}
+                // latitude={Number(place.latitude)}
+                // longitude={Number(place.longitude)}
+                key={i}
+            >
+              {
+                isMobile ? (
+                  <LocationOnOutlinedIcon color="primary" fontSize="large"/>
+                ) : (
+                      <Paper elevation={3} classeName={classes.paper}>
+                        <Typography className={classes.typography} variant="subtitle2" gutterBottom>
+                          {place.name}
+                        </Typography>
+                        <img 
+                            className={classes.pointer}
+                            src={place.photo ? place.photo.images.large.url : "https://images.unsplash.com/photo-1527489377706-5bf97e608852?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8bGFuZHNjYXBlfHx8fHx8MTYzNzg1MzgyNg&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080"}
+                            alt={place.name}
+                            />
+                      </Paper>
+                )
+                }
+            </div>
+         ))}
         <GeolocateControl style={geolocateControlStyle} positionOptions={{ enableHighAccuracy: true }} trackUserLocation={true} auto />
         <Geocoder mapRef={mapRef} onViewportChange={handleGeocoderViewportChange} mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN} position="top-right" marker={false}/>
         <NavigationControl style={navControlStyle} />
@@ -111,6 +136,7 @@ export default function HomeMap({ setCoordinates, setBounds, coordinates }) {
           anchor="top" >
           <div>Hello je suis là</div>
         </Popup>}
+        
       </ReactMapGL>
     </div>
   );
